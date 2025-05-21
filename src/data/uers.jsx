d7f9c9c432fae78b3a747bbd066ecb5b74fec3dd
K33gan123 <127729270+K33gan123@users.noreply.github.com>
2025-05-21 08:24:20 +0200

    Add files via upload

---
+++ src/data/uers.jsx
@@ -0,0 +1,25 @@
+const Users = [
+  {
+    id: 1,
+    name: 'Keegan',
+    email: 'keegan@login.com',
+    password: '123'
+  },
+  {
+    id: 2,
+    name: 'Chris',
+    email: 'chris@login.com',
+    password: '123'
+  },{
+    id: 3,
+    name: 'Nabegh',
+    email: 'Nabegh@login.com',
+    password: '123'
+  },
+  {
+    id: 4,
+    name: 'Ethan',
+    email: 'Ethan@login.com',
+    password: '123'
+  }
+];
