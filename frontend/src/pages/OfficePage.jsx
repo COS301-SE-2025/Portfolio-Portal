96ea64cd44cebab68e53bb333d39018dc049dc65
PawnParser <u22857941@tuks.co.za>
2025-06-26 20:48:45 +0200

    Update OfficePage.jsx

--- frontend/src/pages/OfficePage.jsx
+++ frontend/src/pages/OfficePage.jsx
@@ -1,33 +1,21 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Environment } from '@react-three/drei';
-import Model from '../components/3DModels/Office';
+import OfficeNavbar from "../components/Templates/office/Navbar";
+import OfficeHero from "../components/Templates/office/Hero";
+import OfficeAbout from "../components/Templates/office/About";
+import OfficeExperience from "../components/Templates/office/Experience";
+import OfficeContact from "../components/Templates/office/Contact";
 
 const OfficePage = () => {
   return (
-    <div className="w-full h-screen bg-[#0e0e2c] overflow-hidden">
-      <Canvas
-        camera={{ position: [300, 200, 300], fov: 40 }}
-        style={{ background: '#0e0e2c' }}
-      >
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[100, 100, 100]} intensity={1} />
-        <Suspense fallback={null}>
-          <group position={[0, -100, 0]} scale={0.6}>
-            <Model />
-          </group>
-          <Environment preset="city" />
-          <OrbitControls
-            enablePan={false}
-            minPolarAngle={Math.PI / 4}
-            maxPolarAngle={Math.PI / 2.2}
-            minDistance={150}          //  Prevent zooming in too much
-            maxDistance={750}          //  Prevent zooming out too far
-          />
-        </Suspense>
-      </Canvas>
+    <div className='relative z-0 bg-gray-900 text-white'>
+      <div className='bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900'>
+        <OfficeNavbar />
+        <OfficeHero />
+        <OfficeAbout />
+        <OfficeExperience />
+        <OfficeContact />
+      </div>
     </div>
-  );
-};
+  )
+}
 
 export default OfficePage;
