b0be28670f35b4a12035104ae2a6afccdac714b6
PawnParser <u22857941@tuks.co.za>
2025-06-27 00:39:09 +0200

    Added error handling

--- frontend/src/pages/OfficePage.jsx
+++ frontend/src/pages/OfficePage.jsx
@@ -1,15 +1,23 @@
-import { Navbar, Hero, About, Experience, Contact } from '../components/Templates/office';
-
+import { ErrorBoundary } from "../components/Templates/office";
+import { Navbar, Hero, About, Experience, Contact } from "../components/Templates/office";
 const OfficePage = () => {
   return (
     <div className='relative z-0 bg-gray-900 text-white'>
-      <div className='bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900'>
+      <ErrorBoundary>
         <Navbar />
+      </ErrorBoundary>
+      <ErrorBoundary>
         <Hero />
+      </ErrorBoundary>
+      <ErrorBoundary>
         <About />
+      </ErrorBoundary>
+      <ErrorBoundary>
         <Experience />
+      </ErrorBoundary>
+      <ErrorBoundary>
         <Contact />
-      </div>
+      </ErrorBoundary>
     </div>
   )
 }
