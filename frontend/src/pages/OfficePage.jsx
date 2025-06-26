a4c3e1127211f0d9ba016135630bc0996832d7a7
PawnParser <u22857941@tuks.co.za>
2025-06-26 23:19:11 +0200

    Update OfficePage.jsx

--- frontend/src/pages/OfficePage.jsx
+++ frontend/src/pages/OfficePage.jsx
@@ -1,18 +1,14 @@
-import OfficeNavbar from "../components/Templates/office/Navbar";
-import OfficeHero from "../components/Templates/office/Hero";
-import OfficeAbout from "../components/Templates/office/About";
-import OfficeExperience from "../components/Templates/office/Experience";
-import OfficeContact from "../components/Templates/office/Contact";
+import { Navbar, Hero, About, Experience, Contact } from '../components/Templates/office';
 
 const OfficePage = () => {
   return (
     <div className='relative z-0 bg-gray-900 text-white'>
       <div className='bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900'>
-        <OfficeNavbar />
-        <OfficeHero />
-        <OfficeAbout />
-        <OfficeExperience />
-        <OfficeContact />
+        <Navbar />
+        <Hero />
+        <About />
+        <Experience />
+        <Contact />
       </div>
     </div>
   )
