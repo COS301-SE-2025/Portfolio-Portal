e0908dcc3dbf762b2ab7d4ca3530923514cbfe71
Eric-Booyens <u05127824@tuks.co.za>
2025-09-18 17:47:18 +0200

    added keywords

---
+++ server/app/utils/section-keywords.js
@@ -0,0 +1,153 @@
+// utils/section-keywords.js
+// Keywords for detecting section headers and labels in CVs
+
+module.exports = {
+  name: {
+    inline: [
+      "name",
+      "full name",
+      "first name",
+      "surname",
+      "last name",
+      "family name",
+      "given name",
+    ],
+  },
+  email: {
+    inline: [
+      "email",
+      "email address",
+      "e-mail",
+      "mail",
+      "electronic mail",
+      "gmail",
+      "outlook",
+      "hotmail",
+    ],
+  },
+  phone: {
+    inline: [
+      "phone",
+      "phone number",
+      "mobile",
+      "cell",
+      "telephone",
+      "contact number",
+    ],
+  },
+  linkedin: {
+    inline: ["linkedin", "linkedin profile", "linkedin link"],
+  },
+  website: {
+    inline: [
+      "website",
+      "portfolio",
+      "personal site",
+      "homepage",
+      "github",
+      "gitlab",
+      "site",
+    ],
+  },
+  address: {
+    inline: [
+      "address",
+      "residence",
+      "location",
+      "street",
+      "road",
+      "avenue",
+      "boulevard",
+      "drive",
+      "lane",
+      "city",
+      "town",
+      "province",
+      "state",
+      "postal code",
+      "zip code",
+    ],
+  },
+  about: {
+    inline: [
+      "about",
+      "about me",
+      "summary",
+      "profile",
+      "objective",
+      "career objective",
+      "professional summary",
+      "bio",
+    ],
+  },
+  experience: {
+    inline: [
+      "experience",
+      "work experience",
+      "professional experience",
+      "employment history",
+      "career history",
+      "work history",
+      "professional background",
+    ],
+  },
+  education: {
+    inline: [
+      "education",
+      "academic background",
+      "qualifications",
+      "studies",
+      "training",
+      "degrees",
+      "academics",
+    ],
+  },
+  skills: {
+    inline: [
+      "skills",
+      "skill",
+      "competencies",
+      "proficiencies",
+      "tools",
+      "technologies",
+      "frameworks",
+      "languages",
+      "expertise",
+      "stack",
+      "libraries",
+      "skillset",
+      "specialities",
+      "specializations",
+    ],
+  },
+  certifications: {
+    inline: [
+      "certifications",
+      "certification",
+      "courses",
+      "licenses",
+      "accreditations",
+    ],
+  },
+  languages: {
+    inline: ["languages", "language proficiency", "spoken languages"],
+  },
+  projects: {
+    inline: [
+      "projects",
+      "project experience",
+      "notable projects",
+      "case studies",
+      "personal projects",
+    ],
+  },
+  references: {
+    inline: [
+      "references",
+      "referees",
+      "referee",
+      "references available",
+      "recommendations",
+    ],
+  },
+};
