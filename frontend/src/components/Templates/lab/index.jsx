749f2bb258ab7972fe6de837af702a3d4ab8bad1
PawnParser <u22857941@tuks.co.za>
2025-09-29 02:19:11 +0200

    New copyright button for lab template

--- frontend/src/components/Templates/lab/index.jsx
+++ frontend/src/components/Templates/lab/index.jsx
@@ -7,6 +7,18 @@ export default function LabTemplate() {
     <LabProvider>
       <div className="w-full h-screen relative overflow-hidden">
         <CanvasStage />
+        
+        {/* Copyright Notice Button */}
+        <div className="absolute bottom-4 right-4 z-10">
+          <div className="group relative">
+            <button className="w-8 h-8 rounded-full bg-gray-800 bg-opacity-50 text-white text-xs flex items-center justify-center hover:bg-opacity-70 transition-all">
+              ©
+            </button>
+            <div className="absolute bottom-full right-0 mb-2 w-64 p-3 bg-black bg-opacity-80 text-white text-xs rounded opacity-0 pointer-events-none group-hover:opacity-100 transition-opacity duration-200">
+              "Professor Farnsworth" (https://skfb.ly/oJYTw) by pointlessimon is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+            </div>
+          </div>
+        </div>
       </div>
     </LabProvider>
   );
